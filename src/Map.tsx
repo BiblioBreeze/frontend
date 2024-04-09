799c80e948b442cce7854c79636ff6a1a0f565c0
Stepan Rabotkin <epicstyt@gmail.com>
2024-04-09 16:44:49 +0300

    feat: implement onMarkerPut logic

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,21 +1,49 @@
-import { load } from '@2gis/mapgl';
-import { Map as TMap } from '@2gis/mapgl/types';
+import { load as loadMapGL } from '@2gis/mapgl';
+import { Map as TMap, Marker as TMarker } from '@2gis/mapgl/types';
 import React, { useEffect } from "react";
 
-export const Map = () => {
+export interface Point {
+    longitude: number
+    latitude: number
+}
+
+interface MapProps {
+    onMarkerPut?: (point: Point) => void
+}
+
+export const Map = (props: MapProps) => {
     useEffect(() => {
         let map: TMap;
 
-        load().then((mapglAPI) => {
+        loadMapGL().then((mapglAPI) => {
             map = new mapglAPI.Map('map-container', {
                 center: [55.31878, 25.23584],
                 zoom: 10,
                 key: import.meta.env.VITE_MAPS_API_KEY
             });
+
+            if (props.onMarkerPut) {
+                let marker: TMarker;
+
+                map.on('click', (event) => {
+                    if (marker) {
+                        marker.destroy()
+                    }
+
+                    marker = new mapglAPI.Marker(map, {
+                        coordinates: event.lngLat,
+                    });
+
+                    props.onMarkerPut?.({
+                        longitude: event.lngLat[0],
+                        latitude: event.lngLat[1]
+                    })
+                })
+            }
         });
 
         return () => map && map.destroy();
-    }, []);
+    }, [props]);
 
     return (
         <div style={{ width: '100%', height: '100%' }}>
