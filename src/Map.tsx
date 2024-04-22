b78a9b32bc38eb9f36c463aaaad6cab65f121c07
Stepan Rabotkin <epicstyt@gmail.com>
2024-04-22 03:20:43 +0300

    feat: implement current user geolocation getter at map

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,6 +1,7 @@
-import { load as loadMapGL } from '@2gis/mapgl';
-import { Map as TMap, Marker as TMarker } from '@2gis/mapgl/types';
-import React, { useEffect } from "react";
+import {load as loadMapGL} from '@2gis/mapgl';
+import {Map as TMap, Marker as TMarker} from '@2gis/mapgl/types';
+import React, {useEffect} from "react";
+import userIcon from "./assets/icons/user_at_map.svg"
 
 export interface Point {
     longitude: number
@@ -18,9 +19,23 @@ export const Map = (props: MapProps) => {
         loadMapGL().then((mapglAPI) => {
             map = new mapglAPI.Map('map-container', {
                 center: [55.31878, 25.23584],
-                zoom: 10,
+                zoom: 15,
                 key: import.meta.env.VITE_MAPS_API_KEY
             });
+
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    (success) => {
+                        const coords = [success.coords.longitude, success.coords.latitude]
+
+                        map.setCenter(coords)
+                        new mapglAPI.Marker(map, {
+                            coordinates: coords,
+                            icon: userIcon,
+                        })
+                    },
+                )
+            }
 
             if (props.onMarkerPut) {
                 let marker: TMarker;
@@ -46,15 +61,15 @@ export const Map = (props: MapProps) => {
     }, [props]);
 
     return (
-        <div style={{ width: '100%', height: '100%' }}>
-            <MapWrapper />
+        <div style={{width: '100%', height: '100%'}}>
+            <MapWrapper/>
         </div>
     );
 };
 
 const MapWrapper = React.memo(
     () => {
-        return <div id="map-container" style={{ width: '100%', height: '100%' }}></div>;
+        return <div id="map-container" style={{width: '100%', height: '100%'}}></div>;
     },
     () => true,
 );
